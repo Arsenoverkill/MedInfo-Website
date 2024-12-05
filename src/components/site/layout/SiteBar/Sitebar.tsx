ad4d3667f24fb9f6123b7b3d546eacafdfa7cc1a
adahan <adahanjolchuev@gmail.com>
2024-12-06 02:16:50 +0600

    fullOfSitebar

--- src/components/site/layout/SiteBar/Sitebar.tsx
+++ src/components/site/layout/SiteBar/Sitebar.tsx
@@ -2,10 +2,16 @@
 import Image from "next/image";
 import scss from "./Sitebar.module.scss";
 import user from "@/../public/user.svg";
-import arrow from "@/../public/icons/arrowRigth.svg";
-import logout from "@/../public/icons/iconsLogout.svg";
-import hashtag from "@/../public/icons/хештег.svg";
 import proche from "@/../public/icons/прочее.svg";
+import message from "@/../public/message.svg";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
+import { HiOutlineHashtag } from "react-icons/hi";
+import { BsCapsule } from "react-icons/bs";
+import { RiTestTubeLine } from "react-icons/ri";
+import { IoLayersOutline } from "react-icons/io5";
+import { RiLinksLine } from "react-icons/ri";
+
 
 const SiteBar = () => {
   return (
@@ -22,29 +28,83 @@ const SiteBar = () => {
           />
           <span className={scss.onlineDiv}></span>
         </div>
-        <Image src={proche} alt="proche"/>
+        <Image src={proche} alt="proche" className={scss.proche} />
+        <Image src={message} alt="" className={scss.message} />
+        <h4>DMs</h4>
       </div>
       <div className={scss.userMenu}>
         <span>
           <h3>
-            Отсуствует <Image src={arrow} alt="arrow" className={scss.arrow} />
+            Отсуствует <IoIosArrowForward className={scss.arrow} />
           </h3>
-          <Image src={logout} alt="logout" className={scss.logout} />
+          <RiLogoutBoxRLine className={scss.logout} />
         </span>
         <details className={scss.detailsPersonal}>
-          <summary>Личный кабинет</summary>
+          <summary className={scss.personalTitle}>Личный кабинет</summary>
           <h3>
-            <Image src={hashtag} alt="#" className={scss.hashtag} /> Dashboard
+            <div className={scss.iconTittle}>
+              <HiOutlineHashtag className={scss.hashtag} /> Dashboard{" "}
+            </div>
           </h3>
           <h3>
-            <Image src={hashtag} alt="#" className={scss.hashtag} /> Dashboard
+            <div className={scss.iconTittle}>
+              <HiOutlineHashtag className={scss.hashtag} /> Dashboard
+            </div>
+            <div className={scss.quantity}>1</div>
           </h3>
         </details>
         <details className={scss.detailsPersonal}>
-          <summary>Входящие сообщения</summary>
-          <h3>Мединфо</h3>
-          <h3>Мединфо</h3>
+          <summary className={scss.personalTitle}>Входящие сообщения</summary>
+          <h3>
+            <div className={scss.iconTittle}>
+              {" "}
+              <Image
+                src={user}
+                alt=""
+                width={1000}
+                height={1000}
+                className={scss.userMsg}
+              />
+              Мединфо
+              <span className={scss.onlineDiv}></span>
+            </div>
+            <div className={scss.quantity}>1</div>
+          </h3>
+
+          <h3>
+            <div className={scss.iconTittle}>
+              {" "}
+              <Image
+                src={user}
+                alt=""
+                width={1000}
+                height={1000}
+                className={scss.userMsg}
+              />
+              Мединфо
+              <span className={scss.onlineDiv}></span>
+            </div>
+            <div className={scss.quantity}>444</div>{" "}
+          </h3>
         </details>
+        <span className={scss.userOptions}>
+          <h2>
+            {" "}
+            <BsCapsule className={scss.optionIcons} /> Купить медикаменты
+          </h2>
+          <h2>
+            {" "}
+            <RiTestTubeLine className={scss.optionIcons} /> Сдать анализы
+          </h2>
+          <h2>
+            {" "}
+            <IoLayersOutline className={scss.optionIcons} /> Добавить медисторию
+          </h2>
+          <h2>
+            {" "}
+            <RiLinksLine className={scss.optionIcons} /> Cвязать платформу с
+          </h2>
+        </span>
       </div>
     </div>
   );
