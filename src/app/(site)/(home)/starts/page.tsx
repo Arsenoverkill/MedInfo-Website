6518ec87c0f78aa7959e9928940f05c2592774bd
adahan <adahanjolchuev@gmail.com>
2024-12-12 23:20:43 +0600

    fullStyleSteps

---
+++ src/app/(site)/(home)/starts/page.tsx
@@ -0,0 +1,12 @@
+import StartStep from '@/components/site/page/StartStep'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <StartStep/>
+    </>
+  )
+}
+
+export default page
--- src/app/startStep/steps/page.tsx
+++
@@ -1,3 +0,0 @@
-import Steps from "@/components/site/page/StartSteps/steps/Steps";
-const page = () => <Steps />;
-export default page;
