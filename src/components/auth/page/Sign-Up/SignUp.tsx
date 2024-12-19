81d99bce8a746869b4b777dd0ffaec06d26b4796
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-12-19 19:51:05 +0600

    update sign up choose

--- src/components/auth/page/Sign-Up/SignUp.tsx
+++ src/components/auth/page/Sign-Up/SignUp.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import scss from "./SignUp.module.scss";
 
 const SignUp = () => {
   return (
-    <div>
-      <Link href="/auth/sign-up/user">User</Link>
-      <Link href="/auth/sign-up/med-personal">MedPersonal</Link>
-      <Link href="/auth/sign-up/employee">Employee</Link>
+    <div className={scss.SignUp}>
+      <div className={scss.content}>
+        <Link href="/auth/sign-up/user">User</Link>
+        <Link href="/auth/sign-up/med-personal">MedPersonal</Link>
+        <Link href="/auth/sign-up/employee">Employee</Link>
+      </div>
     </div>
   );
 };
